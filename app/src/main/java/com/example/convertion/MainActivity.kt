2d4fbb35c53f647eae33fa1e644aaae01de1d798
강진우 <wlsdn1252@naver.com>
2023-03-20 18:45:29 +0900

    뷰 바인딩으로 findViewByid를 사용하지 않고 id값을 가져온다

--- app/src/main/java/com/example/convertion/MainActivity.kt
+++ app/src/main/java/com/example/convertion/MainActivity.kt
@@ -2,10 +2,21 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.convertion.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val inputEditText = binding.inputEditText
+        val outputTextView = binding.outputtextView
+        val inputUnitTextView = binding.inputUnitTextView
+        val outputUnitTextView = binding.outputUnitTextView
+
     }
 }
