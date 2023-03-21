ada2b759b79728a8acdff2b85d25cd26bc297208
강진우 <wlsdn1252@naver.com>
2023-03-21 23:24:32 +0900

    화면전환시 데이터가 바뀌는걸 액티비티 라이프 사이클로 추가하여 데이터가 삭제되는 것을 막음

--- app/src/main/java/com/example/convertion/MainActivity.kt
+++ app/src/main/java/com/example/convertion/MainActivity.kt
@@ -2,12 +2,16 @@
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import androidx.core.widget.addTextChangedListener
 import com.example.convertion.databinding.ActivityMainBinding
 
  class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+     var inputNumber : Int = 0
+     var cmToM = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +25,7 @@ import com.example.convertion.databinding.ActivityMainBinding
         val outputUnitTextView = binding.outputUnitTextView
         val swapImageButton = binding.swapImageButton
 
-        var inputNumber : Int = 0
-        var cmToM = true
+
 
         inputEditText.addTextChangedListener{ text ->
             inputNumber = if(text.isNullOrEmpty()){
@@ -50,7 +53,24 @@ import com.example.convertion.databinding.ActivityMainBinding
                 outputTextView.text = inputNumber.times(100).toString()
             }
         }
-
-
     }
+
+     // 화면 회전 시 데이터 유지를 위한 오버라이딩
+     override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+         // bundle에 원하는 데이터 저장
+         outState.putBoolean("cmToM",cmToM)
+
+         super.onSaveInstanceState(outState, outPersistentState)
+     }
+
+     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+         // 위에서 저장된 데이터를 불러온다.
+         cmToM = savedInstanceState.getBoolean("cmToM")
+         
+         // 단순 텍스트 입력
+         binding.inputUnitTextView.text = if(cmToM) "cm" else "m"
+         binding.outputUnitTextView.text = if(cmToM) "m" else "cm"
+
+         super.onRestoreInstanceState(savedInstanceState)
+     }
 }
